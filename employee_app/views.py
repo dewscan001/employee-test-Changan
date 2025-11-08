9523e4b6253c806e5220401851ee532b064e63d6
Dew <62582428+dewscan001@users.noreply.github.com>
2025-11-08 23:16:09 +0700

    to use Browser API of Django rest

--- employee_app/views.py
+++ employee_app/views.py
@@ -1,125 +1,123 @@
-from django.http.response import JsonResponse, HttpResponse
 from .models import *
 from .serializers import *
-from rest_framework.parsers import JSONParser
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
 @csrf_exempt
-@require_http_methods(["GET", "POST"])
-@login_required
+@api_view(["GET", "POST"])
+@login_required    
 def position_list(request):
     if request.method == "GET":
         positionModel = PositionModel.objects.all()
         serializer = PositionSerializer(positionModel, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = PositionSerializer(data=data)
+        
+        serializer = PositionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=201)
         else:
-            return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.errors, status=400)
 
 @csrf_exempt
-@require_http_methods(["GET", "POST"])
-@login_required
+@api_view(["GET", "POST"])
+@login_required    
 def department_list(request):
     if request.method == "GET":
         departmentModel = DepartmentModel.objects.all()
         serializer = DepartmentSerializerGet(departmentModel, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == "POST":
-        data = JSONParser().parse(request)
-        serializer = DepartmentSerializer(data=data)
+        serializer = DepartmentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return Response(serializer.data, status=201)
         else:
-            return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.errors, status=400)
 
 @csrf_exempt
-@require_http_methods(["GET", "POST"])
-@login_required
+@api_view(["GET", "POST"])
+@login_required    
 def employee_list(request):
     if request.method == "GET":
         employeeModel = EmployeeModel.objects.all()
         serializers = EmployeeSerializerGet(employeeModel, many=True)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializers = EmployeeSerializer(data=data)
+        
+        serializers = EmployeeSerializer(data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return JsonResponse(serializers.data, status=201)
+            return Response(serializers.data, status=201)
         else:
-            return JsonResponse(serializers.errors, status=400)
+            return Response(serializers.errors, status=400)
 
 @csrf_exempt      
-@require_http_methods(["GET", "PUT", "DELETE"])
-@login_required
+@api_view(["GET", "PUT", "DELETE"])
+@login_required    
 def position_detail(request, id):
     positionModel = get_object_or_404(PositionModel, pk=id)
 
     if request.method == 'GET':
         serializers = PositionSerializer(positionModel)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
     
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializers = PositionSerializer(positionModel, data=data)
+        
+        serializers = PositionSerializer(positionModel, data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return JsonResponse(serializers.data, status=201)
+            return Response(serializers.data, status=201)
         else:
-            return JsonResponse(serializers.errors, status=400)
+            return Response(serializers.errors, status=400)
     
     elif request.method == 'DELETE':
         positionModel.delete()
-        return HttpResponse(status=204)
+        return Response(status=204)
 
 @csrf_exempt
-@require_http_methods(["GET", "PUT", "DELETE"])
+@api_view(["GET", "PUT", "DELETE"])
 @login_required       
 def department_detail(request, id):
     departmentModel = get_object_or_404(DepartmentModel, pk=id)
     if request.method == 'GET':
         serializers = DepartmentSerializerGet(departmentModel)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializers = DepartmentSerializer(departmentModel, data=data)
+        
+        serializers = DepartmentSerializer(departmentModel, data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return JsonResponse(serializers.data, status=201)
+            return Response(serializers.data, status=201)
         else:
-            return JsonResponse(serializers.errors, status=400)
+            return Response(serializers.errors, status=400)
     elif request.method == 'DELETE':
         departmentModel.delete()
-        return HttpResponse(status=204)
+        return Response(status=204)
 
 @csrf_exempt
-@require_http_methods(["GET", "PUT", "DELETE"])
-@login_required
+@api_view(["GET", "PUT", "DELETE"])
+@login_required    
 def employee_detail(request, id):
     employeeModel = get_object_or_404(EmployeeModel, pk=id)
     if request.method == 'GET':
         serializers = EmployeeSerializerGet(employeeModel)
-        return JsonResponse(serializers.data, safe=False)
+        return Response(serializers.data)
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializers = EmployeeSerializer(employeeModel, data=data)
+        
+        serializers = EmployeeSerializer(employeeModel, data=request.data)
         if serializers.is_valid():
             serializers.save()
-            return JsonResponse(serializers.data, status=201)
+            return Response(serializers.data, status=201)
         else: 
-            return JsonResponse(serializers.errors, status=400)
+            return Response(serializers.errors, status=400)
     elif request.method == 'DELETE':
         employeeModel.delete()
-        return HttpResponse(status=204)
+        return Response(status=204)
