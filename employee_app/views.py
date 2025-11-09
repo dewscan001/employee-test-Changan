9cc85532729a2590ceb52b911034a0b743669777
Dew <62582428+dewscan001@users.noreply.github.com>
2025-11-09 10:01:55 +0700

    refactor

--- employee_app/views.py
+++ employee_app/views.py
@@ -13,11 +13,9 @@ from rest_framework.response import Response
 def position_list(request):
     if request.method == "GET":
         positionModel = PositionModel.objects.all()
-        serializer = PositionSerializer(positionModel, many=True)
-        return Response(serializer.data)
-
+        serializers = PositionSerializer(positionModel, many=True)
+        return Response(serializers.data)
     elif request.method == "POST":
-        
         serializer = PositionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -31,9 +29,8 @@ def position_list(request):
 def department_list(request):
     if request.method == "GET":
         departmentModel = DepartmentModel.objects.all()
-        serializer = DepartmentSerializerGet(departmentModel, many=True)
-        return Response(serializer.data)
-
+        serializers = DepartmentSerializerGet(departmentModel, many=True)
+        return Response(serializers.data)
     elif request.method == "POST":
         serializer = DepartmentSerializer(data=request.data)
         if serializer.is_valid():
@@ -51,33 +48,28 @@ def employee_list(request):
         serializers = EmployeeSerializerGet(employeeModel, many=True)
         return Response(serializers.data)
     elif request.method == 'POST':
-        
-        serializers = EmployeeSerializer(data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=201)
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
         else:
-            return Response(serializers.errors, status=400)
+            return Response(serializer.errors, status=400)
 
 @csrf_exempt      
 @api_view(["GET", "PUT", "DELETE"])
 @login_required    
 def position_detail(request, id):
     positionModel = get_object_or_404(PositionModel, pk=id)
-
     if request.method == 'GET':
-        serializers = PositionSerializer(positionModel)
-        return Response(serializers.data)
-    
+        serializer = PositionSerializer(positionModel)
+        return Response(serializer.data)
     elif request.method == 'PUT':
-        
-        serializers = PositionSerializer(positionModel, data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=201)
+        serializer = PositionSerializer(positionModel, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
         else:
-            return Response(serializers.errors, status=400)
-    
+            return Response(serializer.errors, status=400)
     elif request.method == 'DELETE':
         positionModel.delete()
         return Response(status=204)
@@ -88,16 +80,15 @@ def position_detail(request, id):
 def department_detail(request, id):
     departmentModel = get_object_or_404(DepartmentModel, pk=id)
     if request.method == 'GET':
-        serializers = DepartmentSerializerGet(departmentModel)
-        return Response(serializers.data)
+        serializer = DepartmentSerializerGet(departmentModel)
+        return Response(serializer.data)
     elif request.method == 'PUT':
-        
-        serializers = DepartmentSerializer(departmentModel, data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=201)
+        serializer = DepartmentSerializer(departmentModel, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
         else:
-            return Response(serializers.errors, status=400)
+            return Response(serializer.errors, status=400)
     elif request.method == 'DELETE':
         departmentModel.delete()
         return Response(status=204)
@@ -108,16 +99,15 @@ def department_detail(request, id):
 def employee_detail(request, id):
     employeeModel = get_object_or_404(EmployeeModel, pk=id)
     if request.method == 'GET':
-        serializers = EmployeeSerializerGet(employeeModel)
-        return Response(serializers.data)
+        serializer = EmployeeSerializerGet(employeeModel)
+        return Response(serializer.data)
     elif request.method == 'PUT':
-        
-        serializers = EmployeeSerializer(employeeModel, data=request.data)
-        if serializers.is_valid():
-            serializers.save()
-            return Response(serializers.data, status=201)
+        serializer = EmployeeSerializer(employeeModel, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
         else: 
-            return Response(serializers.errors, status=400)
+            return Response(serializer.errors, status=400)
     elif request.method == 'DELETE':
         employeeModel.delete()
         return Response(status=204)
