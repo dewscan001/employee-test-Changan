9adcc4da7b40a826970ee05dbbe6e32e871e2169
Dew <62582428+dewscan001@users.noreply.github.com>
2025-11-10 12:46:47 +0700

    allow some field can not required

--- employee_app/models.py
+++ employee_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class StatusModel(models.Model):
-    status_name = models.CharField(verbose_name="Status", max_length=255, unique=True, blank=False, null=True)
+    status_name = models.CharField(verbose_name="Status", max_length=255, unique=True)
     def __str__(self):
         return self.status_name
     
@@ -16,16 +16,16 @@ class EmployeeModel(models.Model):
     name = models.CharField(verbose_name="name", max_length=255, blank=False, null=False, unique=True)
     address = models.TextField(verbose_name="address")
     manager = models.BooleanField(verbose_name="manager", default=False)
-    position = models.ForeignKey(verbose_name="position", to='PositionModel', on_delete=models.SET_NULL,  blank=False, null=True)
-    status = models.ForeignKey(verbose_name="status", to='StatusModel', on_delete=models.SET_NULL,  blank=False, null=True)
-    image = models.ImageField(verbose_name="image", upload_to="image", blank=False, null=True)
-    department = models.ForeignKey(verbose_name="department", to='DepartmentModel', on_delete=models.SET_NULL,  blank=False, null=True)
+    position = models.ForeignKey(verbose_name="position", to='PositionModel', on_delete=models.SET_NULL,  blank=True, null=True)
+    status = models.ForeignKey(verbose_name="status", to='StatusModel', on_delete=models.SET_NULL,  blank=True, null=True)
+    image = models.ImageField(verbose_name="image", upload_to="image", blank=True, null=True)
+    department = models.ForeignKey(verbose_name="department", to='DepartmentModel', on_delete=models.SET_NULL,  blank=True, null=True)
     def __str__(self):
         return self.name
 
 class DepartmentModel(models.Model):
     department_name = models.CharField(verbose_name="Department", max_length=255, blank=False, null=False, unique=True)
-    manager = models.OneToOneField(verbose_name="manager", to='EmployeeModel', on_delete=models.SET_NULL,  blank=False, null=True, limit_choices_to={'manager': True})
+    manager = models.OneToOneField(verbose_name="manager", to='EmployeeModel', on_delete=models.SET_NULL,  blank=True, null=True, limit_choices_to={'manager': True})
     def __str__(self):
         return self.department_name
 
